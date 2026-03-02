c3b3f1a521df55b84ddee3a8342a8dc4a5e6b83c
Bizzoibeck <konrad.beck.freiburg@gmail.com>
2026-03-02 17:52:44 +0100

    further work on plots

--- src/visualization/plots.py
+++ src/visualization/plots.py
@@ -1,4 +1,3 @@
-import polars as pl
 import matplotlib.pyplot as plt
 import os 
 from src.model.sandpile import SandpileModel
@@ -6,17 +5,43 @@ from src.model.io import *
 from src.calc.scaling_exponents import *
 import numpy as np 
 import logging
+import pandas as pd
+
+model_id= {
+    "N40d2_open_nonconservative": "A", 
+    "N40d2_open_conservative": "B",
+    "N40d2_closed_nonconservative": "C",
+    "N20d3_open_nonconservative": "D", 
+    "N20d3_open_conservative": "E",
+    "N20d3_closed_nonconservative": "F",
+    "N20d4_open_nonconservative": "G", 
+    "N20d4_open_conservative": "H",
+    "N20d4_closed_nonconservative": "I"
+ 
+}
 
 
-def plot_scaling_exponents(df, model_name, window_size, window_step_size, r_thresh, deviation_factor): 
+latex_conv_table= {
+    "tau": r"$\tau$",
+    "alpha": r"$\alpha$",
+    "lambda": r"$\lambda$",
+    "gamma_1": r"$\gamma_1$",
+    "inv_gamma_1": r"$\gamma_2^{-1}$",
+    "gamma_2": r"$\gamma_1$",
+    "inv_gamma_2": r"$\gamma_2^{-1}$",
+    "gamma_3": r"$\gamma_3$",
+    "inv_gamma_3": r"$\gamma_3^{-1}$"
+ }
+
+def plot_scaling_exponents(df, model_name, window_size, window_step_size, r_thresh, deviation_factor, exponent_data): 
     plot_path = "figures/loglog_plots"
-   
+    data = {}
     exponents = compute_scaling_exponents(df, window_size, window_step_size, r_thresh, deviation_factor)#25,1,0.8,2.0
+    #exponent_data.append(exponents)
     for key, exp in exponents.items():
-    
         values = exp[0]
         parms  = exp[1] 
-        plt.plot(values[0], values[1])
+        plt.plot(values[0], values[1], label= f"P({latex_conv_table[key]})")
         lin_regress_x =np.linspace(np.min(values[0]),np.max(values[0]),10)
         
         if key in ["tau", "alpha", "lambda"]: 
@@ -24,11 +49,17 @@ def plot_scaling_exponents(df, model_name, window_size, window_step_size, r_thre
         else:
             lin_regress_y= lin_regress_x *parms["exponent"]+parms["intercept"]
 
-        plt.plot(lin_regress_x, lin_regress_y)
-        plt.axvline(values[0][parms["lower"]], linestyle=':', linewidth=1)
-        plt.axvline(values[0][parms["upper"]], linestyle=':', linewidth=1)
+        plt.plot(lin_regress_x, lin_regress_y, label= "linear fit")
+        plt.axvline(values[0][parms["lower"]], linestyle=':', linewidth=2, label="bound")
+        plt.axvline(values[0][parms["upper"]], linestyle=':', linewidth=2)
+        plt.legend()
         plt.savefig(f"{plot_path}/{model_name}_{key}.png", dpi=300)
         plt.clf()   
+        data[f"model"]=model_id[model_name]
+        data[f"{latex_conv_table[key]}"]=parms["exponent"]
+    d= pd.DataFrame([data])
+    exponent_data=pd.concat([exponent_data,d])    
+    return exponent_data
 
 
 
@@ -47,36 +78,38 @@ logging.basicConfig(
  
 
 
-def run(filename, window_size, window_step_size, r_thresh, deviation_factor): 
+def run(filename, window_size, window_step_size, r_thresh, deviation_factor, exponent_data): 
     if(filename.endswith(".pkl")): 
         model = load_model(filename)
         df = model.data
+        #print(exponent_data)
     logging.info(f"trying to create log log plots for {filename}")
 
     try: 
-        plot_scaling_exponents(df, filename[:-4], window_size, window_step_size, r_thresh, deviation_factor)
+        exponent_data= plot_scaling_exponents(df, filename[:-4], window_size, window_step_size, r_thresh, deviation_factor, exponent_data)
         logging.info(f"sucess")
     except Exception as e : 
         logging.error(f"failed to create log log plots for {filename}: \n {e}")
+    return exponent_data
+exponent_data= pd.DataFrame() 
 
-run("N40d2_open_nonconservative.pkl",25,1,0.8,2.0)
+exponent_data=run("N40d2_open_nonconservative.pkl",25,1,0.8,2.0, exponent_data)
 
-run("N40d2_open_conservative.pkl",25,1,0.8,2.0)
+exponent_data=run("N40d2_open_conservative.pkl",25,1,0.8,2.0, exponent_data)
 
-run("N40d2_closed_nonconservative.pkl",25,1,0.8,2.0)
+exponent_data=run("N40d2_closed_nonconservative.pkl",25,1,0.8,2.0, exponent_data)
 
-run("N20d3_open_nonconservative.pkl",10,1,0.8,2.0)
+exponent_data=run("N20d3_open_nonconservative.pkl",10,1,0.8,2.0, exponent_data)
 
-run("N20d3_open_conservative.pkl",10,1,0.8,2.0)
+exponent_data=run("N20d3_open_conservative.pkl",10,1,0.8,2.0, exponent_data)
 
-run("N20d3_closed_nonconservative.pkl",10,1,0.8,2.0)
+exponent_data=run("N20d3_closed_nonconservative.pkl",10,1,0.8,2.0, exponent_data)
 
-run("N20d4_open_nonconservative.pkl",10,1,0.8,2.0)
+exponent_data=run("N20d4_open_nonconservative.pkl",5,1,0.8,2.0, exponent_data)
 
-run("N20d4_open_conservative.pkl",10,1,0.8,2.0)
+exponent_data=run("N20d4_open_conservative.pkl",5,1,0.8,2.0,exponent_data)
 
-run("N20d4_closed_nonconservative.pkl",10,1,0.8,2.0)
-
-
+exponent_data=run("N20d4_closed_nonconservative.pkl",5,1,0.8,2.0, exponent_data)
+print(exponent_data.to_latex(buf= "figures/tables/scaling_exp.tex"))
 
 
