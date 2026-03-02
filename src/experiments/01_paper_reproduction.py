e8637125146c017db9a0f03b6fcb32b940017434
Leon Galbas <55129174+lebega@users.noreply.github.com>
2026-03-02 13:30:57 +0100

    Run models for 5 and 6 dims

--- src/experiments/01_paper_reproduction.py
+++ src/experiments/01_paper_reproduction.py
@@ -1,12 +1,13 @@
 import logging
 from itertools import product
+from multiprocessing import Pool
 from os import path
 
 import polars as pl
 
 from src.calc.simulation import run_simulation
 from src.utils import read_config
-from multiprocessing import Pool, cpu_count
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
@@ -18,7 +19,7 @@ logging.basicConfig(
 )
 
 hyperparameters = dict(
-    dims=[(40, 2), (20, 3), (20, 4)],# (15, 5), (10, 10)],
+    dims=[(40, 2), (20, 3), (20, 4)],  # (15, 5), (10, 10)],
     boundary_conditions=["open", "closed"],
     perturbations=["conservative", "nonconservative"],
 )
@@ -28,14 +29,14 @@ outpath = path.join(read_config("data_dir"), "scaling_coefficients.pq")
 measurements = []
 
 
-def run(dim,boundary,perturb):
-#for (N, d), boundary, perturb in parms:  
-    N,d  = dim
+def run(dim, boundary, perturb):
+    # for (N, d), boundary, perturb in parms:
+    N, d = dim
     try:
         run_simulation(N, d, boundary, perturb, n_measure)
         outcome = {
             **hyperparameters,
-            #**coeffs,
+            # **coeffs,
         }
         measurements.append(outcome)
         logging.info(f"Saving scaling exponents to {outpath}")
@@ -46,10 +47,11 @@ def run(dim,boundary,perturb):
             f"Simulation for {N=}, {d=}, {boundary=}, {perturb=}, {n_measure=} failed!\n\tException: {e}."
         )
 
+
 parms = product(
     hyperparameters["dims"],
     hyperparameters["boundary_conditions"],
     hyperparameters["perturbations"],
 )
-with Pool() as pool:          # defaults to number of CPU cores
-        pool.starmap(run, parms)
+with Pool() as pool:  # defaults to number of CPU cores
+    pool.starmap(run, parms)
