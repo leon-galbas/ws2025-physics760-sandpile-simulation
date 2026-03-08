16e4a23f573ac27182b74852905644ee1e455c42
konrad <konrad@MacBook-Pro-von-konrad.local>
2026-03-08 12:48:20 +0100

    Code for animation

---
+++ src/visualization/heat_map_generator.py
@@ -0,0 +1,107 @@
+import logging
+import time
+from itertools import product
+from os import path
+
+import torch
+
+from src.model.io import load_model, model_exists
+from src.model.capture_sandpile import CaptureSandpileModel
+from src.utils import append_dict_to_parquet, read_config
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+def capture(boundary_condition, perturbation,N, time_steps, r= None, burn_in= False): 
+    model = CaptureSandpileModel(
+            N,
+            time_steps,
+            boundary_condition=boundary_condition,
+            perturbation=perturbation,
+            z_init="max"
+        )
+    if burn_in: 
+        model.burn_in(epsilon=0.00397)
+    while model._micro_time< model.capture_time: 
+        model.step(r=r)
+
+    #print(model.sandpile_time_series)
+    return model
+
+
+def plot_heatmaps(model):
+    for z in model.sandpile_time_series: 
+        fig, ax = plt.subplots()
+        im = ax.imshow(z)
+        N = len(z[0])
+        # Show all ticks and label them with the respective list entries
+        ax.set_xticks(range(N), labels=range(N),
+                    rotation=45, ha="right", rotation_mode="anchor")
+        ax.set_yticks(range(len(range(N))), labels=range(N))
+
+        for i in range(N):
+            for j in range(N):
+                text = ax.text(j, i, str(z[i, j].item()),
+                            ha="center", va="center", color="w")
+
+        plt.show()
+
+
+def animate_heatmaps(model, filename): 
+    time_series = model.sandpile_time_series.detach().cpu().numpy()
+    fig, ax = plt.subplots()
+    im = ax.imshow(
+        time_series[0],
+        cmap="hot",
+        vmin=time_series.min(),
+        vmax=time_series.max(),
+        animated= True)
+    T = model.capture_time
+    N = model.N
+    
+    #ax.set_xticks(range(N), labels=range(N),
+    #                    rotation=45, ha="right", rotation_mode="anchor")
+    #ax.set_yticks(range(len(range(N))), labels=range(N))
+    value_texts = []
+
+    def draw_values(frame):
+
+        # remove previous frame's texts
+        for txt in value_texts:
+            txt.remove()
+        value_texts.clear()
+
+        # draw current frame's texts
+        for i in range(N):
+            for j in range(N):
+                txt = ax.text(
+                    j, i,
+                    str(time_series[frame][i, j]),
+                    ha="center", va="center", color="w"
+                )
+                value_texts.append(txt)
+
+    #draw_values(0)
+    def update(frame):
+        
+        im.set_data(time_series[frame])
+        ax.set_title(f"t = {frame}")
+        #draw_values(frame)
+        return [im]
+    
+    ani = FuncAnimation(
+        fig,
+        update,
+        frames=T,
+        interval=17,   # milliseconds between frames
+        blit=True
+    )
+    ani.save(filename, writer="pillow", fps=120)
+model = capture("open", "nonconservative", 25, 1000, burn_in=True)
+animate_heatmaps(model, "figures/heatmaps/test.gif")
